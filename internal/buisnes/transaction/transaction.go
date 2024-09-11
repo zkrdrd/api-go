cb563713bf1a8f1eb1017c27cb2d6cb4a04b2b8f
dev-delphin <eduard.zakirov.96@mail.ru>
2024-09-11 21:41:34 +0500

    db

---
+++ internal/buisnes/transaction/transaction.go
@@ -0,0 +1,51 @@
+package transaction
+
+import "context"
+
+// я тип - я описываю пополнение наличными
+type CacheIn struct {
+	Account string
+	Amount  string
+}
+
+// я тип - я описываю снятие наличными
+type CacheOut struct {
+	Account string
+	Amount  string
+}
+
+// Я тип - я описываю перевод с одного счета на другой
+type Transfer struct {
+	AccountSender    string
+	AccountRecipient string
+	Amount           string
+}
+
+// Я знаю как делать операции с счетами пользователя
+type Accouting struct {
+	// Во мне лежит все  необходимое для работы
+	// к примеру подключение к БД, а возможно и подключения
+	// к другим сервисам
+	db *db.Conn
+}
+
+// Тут я пополняю счет наличными
+func (a *Accouting) CacheOut(ctx context.Context, checkout *CacheOut) error {
+	// Блокирую баланс
+	// Обналичиваю средства
+	// Изменяю сумму баланса
+	// Разблокирую баланс
+	return nil
+}
+
+// Тут я снимаю со счета начличные
+func (a *Accouting) CacheIn(ctx context.Context, checkin *CacheIn) error {
+	//...
+	return nil
+}
+
+// Тут я перевожу деньги между внетренними счетами
+func (a *Accouting) InternalTransfer(ctx context.Context, transfer *Transfer) error {
+	//...
+	return nil
+}
