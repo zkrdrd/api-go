7825937244fff8d9ad4862dce803963ee25d933b
dev-delphin <eduard.zakirov.96@mail.ru>
2024-10-15 22:28:45 +0500

    handlers

---
+++ pkg/service/handlers.go
@@ -0,0 +1,144 @@
+package service
+
+import (
+	"api-go/pkg/models"
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+const (
+	MsgInvalidBody = `invalid body`
+)
+
+func handlersCashOut(method string, fn func(context.Context, *models.CashOut) error) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if method != r.Method {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		cashOut := &models.CashOut{}
+
+		if err := json.NewDecoder(r.Body).Decode(cashOut); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(MsgInvalidBody))
+			return
+		}
+
+		if err := fn(r.Context(), cashOut); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func handlersCashIn(method string, fn func(context.Context, *models.CashIn) error) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if method != r.Method {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		cashIn := &models.CashIn{}
+
+		if err := json.NewDecoder(r.Body).Decode(cashIn); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(MsgInvalidBody))
+			return
+		}
+
+		if err := fn(r.Context(), cashIn); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func handlersInternalTransfer(method string, fn func(context.Context, *models.InternalTranser) error) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if method != r.Method {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		internalTransfer := &models.InternalTranser{}
+
+		if err := json.NewDecoder(r.Body).Decode(internalTransfer); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(MsgInvalidBody))
+			return
+		}
+
+		if err := fn(r.Context(), internalTransfer); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func handlersGerTransaction(method string, fn func(id string) (*models.Transactions, error)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if method != r.Method {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		internalTransfer := &models.InternalTranser{}
+
+		if err := json.NewDecoder(r.Body).Decode(internalTransfer); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(MsgInvalidBody))
+			return
+		}
+
+		_, err := fn(internalTransfer.AccountSender)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func handlersGetBalance(method string, fn func(id string) (*models.Balance, error)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if method != r.Method {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		balanceId := &models.Balance{}
+
+		if err := json.NewDecoder(r.Body).Decode(balanceId); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, _ = w.Write([]byte(MsgInvalidBody))
+			return
+		}
+
+		_, err := fn(balanceId.Account)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(err.Error()))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,23 +1,23 @@
 package service
 
 import (
-	"context"
-	"io"
-	"log"
+	"api-go/internal/postgredb"
+	"api-go/internal/services"
 	"net/http"
 )
 
-func CallLogic(fn func(context.Context, []byte) ([]byte, error)) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		buf, _ := io.ReadAll(r.Body)
+func Handlers(accounting *services.Accouting,
+	storage postgredb.Storage) *http.ServeMux {
+	mux := &http.ServeMux{}
 
-		// fn for call
-		res, err := fn(r.Context(), buf)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			log.Print(err)
-		}
+	//http.HandleFunc(`/v1/accounting/balance`, handlersGetAllBalances(http.MethodGet, accounting.CashOut)) - no method ListAccountinBalance
+	http.HandleFunc(`/v1/accounting/balance/{id}`, handlersGetBalance(http.MethodGet, storage.GetAccountBalance))
 
-		w.Write(res)
-	}
+	//http.HandleFunc(`/v1/accounting/transaction`, handlersGerAllTransactions(http.MethodGet, accounting.ListInternalTransaction)) - how add Filter
+	http.HandleFunc(`/v1/accounting/transaction/{id}`, handlersGerTransaction(http.MethodGet, storage.GetInternalTrasaction))
+	http.HandleFunc(`/v1/accounting/transaction/cash-out`, handlersCashOut(http.MethodPost, accounting.CashOut))
+	http.HandleFunc(`/v1/accounting/transaction/cash-in`, handlersCashIn(http.MethodPost, accounting.CashIn))
+	http.HandleFunc(`/v1/accounting/transaction/internal-transfer`, handlersInternalTransfer(http.MethodPost, accounting.InternalTransfer))
+
+	return mux
 }
